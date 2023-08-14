226b8a1a9ce4d565952bbefd16c3b0226b8b817f
Krisco22 <116952759+Krisco22@users.noreply.github.com>
2023-08-14 23:28:53 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DATA SCIENCE TOOLS AND ECOSYSTEM
+
+# In this notebook, we will go through the fundamentals of data science tools that are popular 
+
+# Three popular languages that are used in data science are:
+#   1. Python
+#   2. SQL
+#   3. R
+
+# Some of the popular data science libraries are:
+#  1. Pandas
+#  2. NumPy
+#  3. sciKit-learn
+#  4. Keras
+#  5. PyTorch
+#  
+
+# |Data science tools|
+# |---------|
+# | RStudio |
+# | Apache Spark |
+# | TensorFlow |
+
+# Below are few examples of evaluating arithmetic expressions in python
+
+# In[2]:
+
+
+51*62
+
+
+# In[7]:
+
+
+(1+65)-1
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[8]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[10]:
+
+
+mins = 200
+hours = mins/60
+print(hours)
+
+
+# ## Objectives:
+
+# - List popular libraries
+# - List Popular ds languages
+# - Convert minutes to hours
+# - Evaluate arithmetic expressions
+
+# ## AUTHOR:
+# Krishnan Narayanan
+
+# In[ ]:
+
+
+
+
